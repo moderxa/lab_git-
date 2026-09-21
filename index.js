33971f1a1325509fe0985d45b04db237288a62f1
moderxa <oprohorova347@gmail.com>
2026-09-21 11:59:12 +0300

    Реализовано возведение в степень

---
+++ index.js
@@ -0,0 +1,1 @@
+let num1 = 0, num2 = 0; function processChoice(c) { if (c === "5") console.log("Результат возведения в степень: " + Math.pow(num1, num2)); else console.log("Операция не реализована в этой ветке"); }
